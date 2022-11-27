16b6a7f280c33c8b4c6ab78d48a5ac19b3b0d540
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-11-27 23:47:25 +0700

    Scrape EPL from season 22/23 to 20/21

--- scrapers/scrapers/spiders/epl_spider.py
+++ scrapers/scrapers/spiders/epl_spider.py
@@ -9,14 +9,17 @@ import scrapy
 from scrapy_selenium import SeleniumRequest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--window-size=1280,720")
 
 from ..items import *
-from itemadapter import ItemAdapter
 
 class EPLSpider(scrapy.Spider):
-    name = "eplmatch"
+    name = "eplbot"
     custom_settings = {
         'ITEM_PIPELINES': {
             'scrapers.pipelines.JsonWriter': 100
@@ -24,54 +27,70 @@ class EPLSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        url = "https://www.premierleague.com/results"
+        url = "https://www.premierleague.com/match/58968"
         yield SeleniumRequest(
             url=url,
-            callback=self.parse,
+            callback=self.parse_match,
         )
     
-    def __init__(self, timeout=10):
-        self.driver = webdriver.Chrome(os.path.join(SCRAPERS_ROOT, "chromedriver.exe"))
-        self.wait = WebDriverWait(self.driver, timeout)
-        
+    def __init__(
+        self, timeout=10, 
+        seasons=["2020/21"] # "2022/23", "2021/22", "2020/21", "2019/20"
+        ):
         self.BASE_URL = "https://www.premierleague.com/match/"
         self.rolemap = {0: "gk", 1: "df", 2: "mf", 3: "fw"}
         self.timeout = timeout
+        self.seasons = seasons
+        self.savepoint = 'eplbot.jsonl'
 
     def parse(self, response):
-        self.driver.get(response.url)
-        time.sleep(5)
-        
-        # accept cookie request
-        cookie_locator = 'body > div.tcf-cmp._1Qu7MokjMuBXLOM2oKVLhZ._3_H6MsAd1grAO7T3v2WdhQ > div > div > div._1QkG3L-zAijqYlFASTvCtT > div._24Il51SkQ29P1pCkJOUO-7 > button._2hTJ5th4dIYlveipSEMYHH.BfdVlAo_cgSVjDUegen0F.js-accept-all-close'
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, cookie_locator)))
-        self.driver.find_element(By.CSS_SELECTOR, cookie_locator).click()
-        
-        # scroll to the end of the page -> load all matches
-        new_height = self.driver.execute_script("return document.body.scrollHeight")
-        while True:
-            self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-            time.sleep(1)
-            prev_height = new_height
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
-            if new_height == prev_height:
-                break
-        
-        # wait the div.loader detached off DOM
-        loader_locator = '#mainContent > div.tabbedContent > div.wrapper.col-12.tabLoader.active > div:nth-child(3) > section > div.loader'
-        self.wait.until_not(EC.presence_of_element_located((By.CSS_SELECTOR, loader_locator)))
+        # filter the season
+        for season in self.seasons:
+            driver = webdriver.Chrome(
+                os.path.join(SCRAPERS_ROOT, "chromedriver.exe"), 
+                chrome_options=chrome_options)
+            wait = WebDriverWait(driver, self.timeout)
+            
+            driver.get(response.url)
+            time.sleep(5)
+            
+            # accept cookie request
+            cookie_locator = 'body > div.tcf-cmp._1Qu7MokjMuBXLOM2oKVLhZ._3_H6MsAd1grAO7T3v2WdhQ > div > div > div._1QkG3L-zAijqYlFASTvCtT > div._24Il51SkQ29P1pCkJOUO-7 > button._2hTJ5th4dIYlveipSEMYHH.BfdVlAo_cgSVjDUegen0F.js-accept-all-close'
+            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, cookie_locator)))
+            driver.find_element(By.CSS_SELECTOR, cookie_locator).click()
 
-        # find & send requests for the match page
-        fixtures = self.driver.find_elements(By.XPATH, '//*[@id="mainContent"]/div[3]/div[1]/div[2]/section/div[@class="fixtures__matches-list"]')
-        for fixture in fixtures:
-            matches = fixture.find_elements(By.XPATH, '//*[@class="matchList"]/*[@class="matchFixtureContainer"]')
-            for match in matches:
-                yield SeleniumRequest(
-                    url=self.BASE_URL + match.get_attribute("data-comp-match-item"),
-                    callback=self.parse_match
-                )
+            # choose season
+            driver.execute_script("window.scrollTo(0,0);")
+            wait.until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[3]/div[1]/section/div[@data-dropdown-block="compSeasons"]'))).click()
+            wait.until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[3]/div[1]/section/div[@data-dropdown-block="compSeasons"]/ul[@class="dropdownList"]/li[@data-option-name="{season}"]'))).click()
+
+            # scroll to the end of the page -> load all matches
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            while True:
+                driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+                time.sleep(2)
+                prev_height = new_height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == prev_height:
+                    break
+            
+            # wait the div.loader detached off DOM
+            loader_locator = '#mainContent > div.tabbedContent > div.wrapper.col-12.tabLoader.active > div:nth-child(3) > section > div.loader'
+            wait.until_not(EC.presence_of_element_located((By.CSS_SELECTOR, loader_locator)))
+
+            # find & send requests for the match page
+            fixtures = driver.find_elements(By.XPATH, '//*[@id="mainContent"]/div[3]/div[1]/div[2]/section/div[@class="fixtures__matches-list"]')
+            for fixture in fixtures:
+                matches = fixture.find_elements(By.XPATH, '//*[@class="matchList"]/*[@class="matchFixtureContainer"]')
+                for match in matches:
+                    yield SeleniumRequest(
+                        url=self.BASE_URL + match.get_attribute("data-comp-match-item"),
+                        callback=self.parse_match,
+                        cb_kwargs=dict(season=season)
+                    )
+                    
     
-    def parse_match(self, response):
+    def parse_match(self, response, season="2020/21"):
         driver = webdriver.Chrome(os.path.join(SCRAPERS_ROOT, "chromedriver.exe"))
         wait = WebDriverWait(driver, self.timeout)
         driver.get(response.url)
@@ -104,7 +123,8 @@ class EPLSpider(scrapy.Spider):
         yield Match(
             home_team=home,
             away_team=away,
-            home_result="win" if goals[0] > goals[1] else ("draw" if goals[0] == goals[1] else "lose") 
+            home_result="win" if goals[0] > goals[1] else ("draw" if goals[0] == goals[1] else "lose"),
+            season=season
         )
 
     def __parse_team_container(self, team_container):

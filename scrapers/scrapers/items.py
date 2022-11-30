d30c9cdbf1593de4bf681e6c258a1926cda9fb37
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-11-30 13:28:18 +0700

    Scrape FPL for player performance data

--- scrapers/scrapers/items.py
+++ scrapers/scrapers/items.py
@@ -8,6 +8,28 @@ from scrapy import Item, Field
 class Player(Item):
     name = Field(serializer=str)
     id = Field(serializer=int)
+    season = Field(serializer=str)
+    INDICES = ('pts', 'mp', 'gs', 'a', 'cs', 'gc', 'og', 'ps', 'pm', 'yc', 'rc', 's', 'b', 'bps', 'i', 'c', 't', 'ii')
+    
+    pts = Field(serializer=float)
+    mp = Field(serializer=float)
+    gs = Field(serializer=float)
+    a = Field(serializer=float)
+    cs = Field(serializer=float)
+    gc = Field(serializer=float)
+    og = Field(serializer=float)
+    ps = Field(serializer=float)
+    pm = Field(serializer=float)
+    yc = Field(serializer=float)
+    rc = Field(serializer=float)
+    s = Field(serializer=float)
+    b = Field(serializer=float)
+    bps = Field(serializer=float)
+
+    i = Field(serializer=float)
+    c = Field(serializer=float)
+    t = Field(serializer=float)
+    ii = Field(serializer=float)
 
 class LineUp(Item):
     gk = Field(serializer=list)

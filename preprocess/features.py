f9b61533b35145dce26baa40d18560c61a3143d8
dat <dat27072002@gmail.com>
2023-01-04 02:54:42 +0700

    integrate data

---
+++ preprocess/features.py
@@ -0,0 +1,23 @@
+all_pos = [
+    'home/gk_0',
+    'home/df_0', 'home/df_1', 'home/df_2', 'home/df_3', 'home/df_4',
+    'home/mf_0', 'home/mf_1', 'home/mf_2', 'home/mf_3', 'home/mf_4',
+    'home/fw_0', 'home/fw_1', 'home/fw_2',
+    'away/gk_0', 'away/df_0', 'away/df_1', 'away/df_2', 'away/df_3',
+    'away/mf_0', 'away/mf_1', 'away/mf_2', 'away/mf_3', 'away/mf_4',
+    'away/fw_0', 'away/fw_1', 'away/fw_2',
+]
+
+all_pos_home = ['home/name', 'away/name', 'home/gk_0', 'home/df_0', 'home/df_1',
+                'home/df_2', 'home/df_3', 'home/df_4', 'home/mf_0', 'home/mf_1',
+                'home/mf_2', 'home/mf_3', 'home/mf_4', 'home/fw_0', 'home/fw_1',
+                'home/fw_2']
+
+columns = ['home/name', 'away/name', 'home/gk_0',
+           'home/df_0', 'home/df_1', 'home/df_2', 'home/df_3', 'home/df_4',
+           'home/mf_0', 'home/mf_1', 'home/mf_2', 'home/mf_3', 'home/mf_4',
+           'home/fw_0', 'home/fw_1', 'home/fw_2',
+           'away/gk_0', 'away/df_0', 'away/df_1', 'away/df_2', 'away/df_3',
+           'away/mf_0', 'away/mf_1', 'away/mf_2', 'away/mf_3', 'away/mf_4',
+           'away/fw_0', 'away/fw_1', 'away/fw_2',
+           'home_result', 'season']

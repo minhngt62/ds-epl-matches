012975fe7c6d0e5d3f31a2a64d536ae089fc8051
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-01-04 23:22:08 +0700

    Crawl URLs of players in EPL
    Number of players: ~2068

--- scrapers/scrapers/spiders/player_spider.py
+++ scrapers/scrapers/spiders/player_spider.py
@@ -19,8 +19,8 @@ chrome_options.add_argument("--window-size=1280,1020")
 
 from ..items import *
 
-class PlayerSpider:
-    name = "playerbot"
+class PlayerSpider(scrapy.Spider):
+    name = "players"
     custom_settings = {
         'ITEM_PIPELINES': {
             'scrapers.pipelines.PlayerDefaultFields': 50,
@@ -37,59 +37,57 @@ class PlayerSpider:
 
     def __init__(
         self,
-        seasons: List[str] = ["2019/20", "2020/21", "2021/22"]
+        seasons: List[str] = ["2019/20", "2020/21", "2021/22"],
+        preload: str = "scrapers\url_players.jsonl"
         ):
         self.seasons = seasons
         self.output = "players.jsonl"
         self.load_relay = 3
+        self.preload = preload
 
         self.crawls: List[Dict[str, str]] = [] # {"url": url, "season": season}
 
     def crawl(self, response) -> None:
-        for season in self.seasons:
-            driver = webdriver.Chrome(
-                os.path.join(SCRAPERS_ROOT, "chromedriver.exe"), 
-                chrome_options=chrome_options
-            )
-            
-            driver.get(response.url)
-            time.sleep(self.load_relay)
-
-            cookie_path = "body > div.tcf-cmp._1Qu7MokjMuBXLOM2oKVLhZ._3_H6MsAd1grAO7T3v2WdhQ > div > div > div._1QkG3L-zAijqYlFASTvCtT > div._24Il51SkQ29P1pCkJOUO-7 > button._2hTJ5th4dIYlveipSEMYHH.BfdVlAo_cgSVjDUegen0F.js-accept-all-close"
-            self._accept_cookie(cookie_path)
-
-            driver.execute_script("window.scrollTo(0,0);")
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[3]/div[1]/section/div[@data-dropdown-block="compSeasons"]'))).click()
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[3]/div[1]/section/div[@data-dropdown-block="compSeasons"]/ul[@class="dropdownList"]/li[@data-option-name="{season}"]'))).click()
-
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            while True:
-                driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-                time.sleep(self.load_relay)
-                prev_height = new_height
-                new_height = driver.execute_script("return document.body.scrollHeight")
-                if new_height == prev_height:
-                    break
-            loader_path = '#mainContent > div.tabbedContent > div.wrapper.col-12.tabLoader.active > div:nth-child(3) > section > div.loader'
-            WebDriverWait(driver, 20).until_not(EC.presence_of_element_located((By.CSS_SELECTOR, loader_path)))
-
-            player_containers = driver.find_elements(By.CSS_SELECTOR, "#mainContent > div.playerIndex > div.wrapper > div > div > table > tbody > tr")
-            for player in player_containers:
-                position = player.find_element(By.CSS_SELECTOR, "td:nth-child(2) > a")
-                url = player.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a")
-                self.crawls.append(
-                    {
-                        "url": url.get_attribute("href").lstrip("//").replace("overview", "stats"),
-                        "season": season,
-                        "postion": position.text
-                    }
+        if not self.preload:
+            for season in self.seasons:
+                driver = webdriver.Chrome(
+                    os.path.join(SCRAPERS_ROOT, "chromedriver.exe"), 
+                    chrome_options=chrome_options
                 )
+                
+                driver.get(response.url)
+                time.sleep(self.load_relay)
+
+                self._accept_cookie(driver, "body > div.tcf-cmp._1Qu7MokjMuBXLOM2oKVLhZ._3_H6MsAd1grAO7T3v2WdhQ > div > div > div._1QkG3L-zAijqYlFASTvCtT > div._24Il51SkQ29P1pCkJOUO-7 > button._2hTJ5th4dIYlveipSEMYHH.BfdVlAo_cgSVjDUegen0F.js-accept-all-close")
+                self._close_adv(driver, "#advertClose")
+                
+                driver.execute_script("window.scrollTo(0,0);")
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[2]/div[1]/div/section/div[@data-dropdown-block="compSeasons"]'))).click()
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[2]/div[1]/div/section/div[@data-dropdown-block="compSeasons"]/ul[@class="dropdownList"]/li[@data-option-name="{season}"]'))).click()
+
+                self._scroll_to_bottom(driver)
+                loader_path = '#mainContent > div.tabbedContent > div.wrapper.col-12.tabLoader.active > div:nth-child(3) > section > div.loader'
+                WebDriverWait(driver, 20).until_not(EC.presence_of_element_located((By.CSS_SELECTOR, loader_path)))
+
+                player_containers = driver.find_elements(By.CSS_SELECTOR, "#mainContent > div.playerIndex > div.wrapper > div > div > table > tbody > tr")
+                for player in player_containers:
+                    position = player.find_element(By.CSS_SELECTOR, "td:nth-child(2)")
+                    url = player.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a")
+                    self.crawls.append(
+                        {
+                            "url": url.get_attribute("href").lstrip("//").replace("overview", "stats"),
+                            "season": season,
+                            "postion": position.text
+                        }
+                    )
+            self._save_urls()
+            return
         
-        self._save_urls()
-        self._parse()
+        with open(self.preload, "r") as f:
+            self.crawls: List[Dict[str, str]] = json.load(f)
+            self._parse()
     
     def _parse(self):
-        cookie_path = "body > div.tcf-cmp._1Qu7MokjMuBXLOM2oKVLhZ._3_H6MsAd1grAO7T3v2WdhQ > div > div > div._1QkG3L-zAijqYlFASTvCtT > div._24Il51SkQ29P1pCkJOUO-7 > button._2hTJ5th4dIYlveipSEMYHH.BfdVlAo_cgSVjDUegen0F.js-accept-all-close"
         for crawl in self.crawls:
             driver = webdriver.Chrome(
                 os.path.join(SCRAPERS_ROOT, "chromedriver.exe"), 
@@ -99,20 +97,36 @@ class PlayerSpider:
             driver.get(crawl["url"])
             time.sleep(self.load_relay)
             
-            self._accept_cookie(cookie_path)
-            adclose_path = "#advertClose"
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, adclose_path))).click()
+            self._accept_cookie(driver, "body > div.tcf-cmp._1Qu7MokjMuBXLOM2oKVLhZ._3_H6MsAd1grAO7T3v2WdhQ > div > div > div._1QkG3L-zAijqYlFASTvCtT > div._24Il51SkQ29P1pCkJOUO-7 > button._2hTJ5th4dIYlveipSEMYHH.BfdVlAo_cgSVjDUegen0F.js-accept-all-close")
+            self._close_adv(driver, "#advertClose")
 
             driver.execute_script("window.scrollTo(0,0);")
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[3]/div/div/div[2]/div/div/section/div[@data-dropdown-block="compSeasons"]'))).click()
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="mainContent"]/div[3]/div/div/div[2]/div/div/section/div[@data-dropdown-block="compSeasons"]/ul[@class="dropdownList"]/li[@data-option-name="{crawl["season"]}"]'))).click()
 
-            pass
-
     def _save_urls(self) -> None:
-        with open("player_urls.json", "w") as f:
+        with open("url_" + self.output, "w") as f:
             json.dump(self.crawls, f)
             
     def _accept_cookie(self, driver: webdriver.Chrome, path):
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, path)))
-        driver.find_element(By.CSS_SELECTOR, path).click()
+        try:
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, path)))
+            driver.find_element(By.CSS_SELECTOR, path).click()
+        except:
+            pass
+    
+    def _close_adv(self, driver: webdriver.Chrome, path):
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, path))).click()
+        except:
+            pass
+    
+    def _scroll_to_bottom(self, driver: webdriver.Chrome):
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+            time.sleep(self.load_relay)
+            prev_height = new_height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == prev_height:
+                break
